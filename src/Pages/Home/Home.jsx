d9e45b29a0ae45b0aeb6df4643b6275253e5c389
Daniel Suarez <danielsuarez3114@gmail.com>
2023-02-24 17:56:05 -0500

    init projects

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+export const Home = () => {
+  return (
+    <div>
+    <header className='head'>
+        <h1>Home</h1>
+    </header>
+    <main>
+        <p>Home page</p>
+    </main>
+    </div>
+  )
+}
