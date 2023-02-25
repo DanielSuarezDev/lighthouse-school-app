3679752d62bd5f1c9ffead1ea677245da5ca67bf
Samuel Suarez <samuelsuarez.lhs@gmail.com>
2023-02-24 19:04:22 -0500

    homedesign

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,9 +7,13 @@ export const Home = () => {
     <header className='head'>
         <h1>Home</h1>
     </header>
-    <main>
-        <p>Home page</p>
-    </main>
+      <div clas="Logo">Logo</div>
+      <div class="Estudiante">Estudiante</div>
+      <div class="Maestro">Maestro</div>
+      <div class="Contactos">Contactos</div>
+      <div class="Calendario">Calendario</div>
+      <div class="Inicio">Inicio</div>
+      <div class="Tareas">Tareas</div>
     </div>
   )
 }
